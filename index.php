a23a6183dc12287981e83e32ad0a145418ac11cf
Matheus Ledo <matheus.ledo@grupointegrado.br>
2026-05-26 20:45:14 -0300

    Criação inicio páginas + HTML + CSS

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Z&L Cars</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <a class="a_fora_nav" href="index.php" title="Página Inicial">Z&L Cars</a>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="produtos.php">Produtos</a></li>
+                <li><a href="conta.php">Conta</a></li>
+                <li><a href="contato.php">Contato</a></li>
+                <li><a href="carrinho.php">Carrinho</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="home">
+            <div class="quem_somos">
+        <h1>Quem somos?</h1>
+     <p>Somos a Z&L Cars, uma empresa especializada na compra e venda de veículos, localizada em Corumbataí do Sul - PR, oferecemos soluções completas para clientes pessoa física (PF) e pessoa jurídica (PJ). Atuamos com transparência, segurança e compromisso, buscando sempre proporcionar as melhores oportunidades do mercado automotivo.<br>
+Trabalhamos com uma ampla variedade de veículos, incluindo carros de passeio, motos, utilitários e veículos comerciais, atendendo às diferentes necessidades e aos diferentes perfis de clientes. Nosso objetivo é garantir qualidade, procedência e excelentes condições de negociação em cada venda.<br>
+Na Z&L Cars, valorizamos o atendimento personalizado e a confiança construída com cada cliente, oferecendo suporte desde a escolha do veículo até a finalização da compra ou venda.</p>
+</div>
+        </section>
+        </main>
+    <footer>
+    <p>2026 - Z&L Cars</p>
+</footer>
+</body>
+</html>
